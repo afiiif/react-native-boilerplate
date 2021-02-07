a9d96775d4dbda1af42b27825da2d69d4d6773a6
Muhammad Afifudin <afif.x.udin@gmail.com>
2021-02-07 20:39:04 +0700

    Setup ESLint & husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'no-console': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'import/prefer-default-export': 'off',
+    'react-native/no-unused-styles': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+  },
 };
